077eecc7e6550857de87ead5b2c8149019d6da69
forsterj <julian.forster@haufe-lexware.com>
2017-08-07 09:48:21 +0200

    Revert "test123"
    This reverts commit de9f72d5e779a2fa3537d82e2799f9f023421587.

--- js/app.js
+++ js/app.js
@@ -120,7 +120,7 @@ var App = Backbone.View.extend({
     },
     activateMap: function($event){
 
-        console.log('activateMap 2');
+        console.log('activateMap');
 
         if(this.map_active) return;
 
@@ -142,7 +142,7 @@ var App = Backbone.View.extend({
 
     },
     resetMap: function(){
-        console.log('resetMap 2');
+        console.log('resetMap');
         this.origin_y = 0;
         this.origin_x = 0;
         this.map_active = false;
