2fdc6355f2daa445b4f2bd8b464f036fff0e54ba
forsterj <julian.forster@haufe-lexware.com>
2017-08-07 10:13:44 +0200

    test

--- js/app.js
+++ js/app.js
@@ -140,7 +140,7 @@ var App = Backbone.View.extend({
 
     },
     resetMap: function(){
-        console.log('resetMap 6');
+        console.log('resetMap 7');
         this.origin_y = 0;
         this.origin_x = 0;
         this.map_active = false;
